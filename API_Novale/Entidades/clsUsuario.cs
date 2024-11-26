e0a2f0cb1b72d1861049d6be765283b345c859bf
IsaKatha98 <isabel.loerzer@iesnervion.es>
2024-11-26 13:35:55 +0100

    me suicido ya, no?

---
+++ API_Novale/Entidades/clsUsuario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clsUsuario
+    {
+        #region atributos
+        private int idUsuario;
+        private string nombreUsu;
+        private string correo;
+        private string password;
+        private string foto;
+        #endregion
+
+        #region constructores
+        public clsUsuario()
+        {
+            idUsuario = 0;
+            nombreUsu = string.Empty;
+            correo = string.Empty;
+            password = string.Empty;
+            foto = string.Empty;
+        }
+
+        public clsUsuario(int idUsuario, string nombreUsu, string apellidos, string correo, string password, DateTime fechaNac, string foto)
+        {
+            this.idUsuario = idUsuario;
+            this.nombreUsu = nombreUsu;
+            this.correo = correo;
+            this.password = password;
+            this.foto = foto;
+        }
+        #endregion
+
+        #region propiedades
+        public int IdUsuario {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        public string NombreUsu {
+            get { return nombreUsu; }
+            set { nombreUsu = value; }
+        }
+
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Foto
+        {
+            get { return foto; }
+            set { foto = value; }
+        }
+
+        #endregion
+
+    }
+}
